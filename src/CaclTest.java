86e1b3b0e0b72f54f9cc37280a2211c2cb7071b8
mateu <mateusz.wygoda>
2022-01-30 22:08:54 +0100

    Add test for methods

--- src/CaclTest.java
+++ src/CaclTest.java
@@ -1,5 +1,16 @@
 public class CaclTest {
     public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        double a = 10;
+        double b = 3;
+        double c = 1;
+        double d = 4;
 
+        System.out.println(calc.add(a,b));
+        System.out.println(calc.add(a,b,c));
+        System.out.println(calc.add(a,b,c,d));
+        System.out.println(calc.subtract(a,b));
+        System.out.println(calc.subtract(a,b,c));
+        System.out.println(calc.subtract(a,b,c,d));
     }
 }
