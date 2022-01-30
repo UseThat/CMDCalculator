a13c83ee3fb281082b578ebe47243d59afa7f51a
mateu <mateusz.wygoda>
2022-01-30 22:06:56 +0100

    Create methods Add and subtract

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,2 +1,23 @@
 public class Calculator {
+    double add(double a, double b){
+        return a + b;
+    }
+    double add(double a, double b, double c){
+        return add(a,b) + c;
+    }
+
+    double add(double a, double b, double c,double d) {
+        return add(a, b, c) + d;
+    }
+
+    double subtract(double a, double b){
+        return a - b;
+    }
+    double subtract(double a, double b, double c){
+        return add(a,b) - c;
+    }
+
+    double subtract(double a, double b, double c,double d) {
+        return add(a, b, c) - d;
+    }
 }
